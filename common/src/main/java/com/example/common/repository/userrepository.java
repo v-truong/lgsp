b062d5a8d5bc96b5234e2803c0241104e3750aea
truong <toi123diboi@gmail.com>
2023-05-07 14:28:58 +0700

    day lai code

--- common/src/main/java/com/example/common/repository/userrepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.common.repository;
-
-import com.example.common.entity.user;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface userrepository extends JpaRepository<user,Long> {
-}
--- common/src/main/java/com/example/common/service/userservice.java
+++
@@ -1,9 +0,0 @@
-package com.example.common.service;
-
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class userservice {
-    public static  final Logger =LoggerFactory.getLo
-}
